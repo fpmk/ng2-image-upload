88b4fe2ca57d8da7bdb27f74e7a78e6ce8725fdb
fpmk1122@gmail.com <2fq3eatb>
2018-10-01 16:31:51 +0700

    lib fix

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-export { ImageUploadModule } from './lib/dist/image-upload.module';
-export { ImageUploadComponent, FileHolder } from './lib/dist/image-upload/image-upload.component';
-export { UploadMetadata } from './lib/dist/image-upload/before-upload.interface';
+export { ImageUploadModule } from './lib/image-upload.module';
+export { ImageUploadComponent, FileHolder } from './lib/image-upload/image-upload.component';
+export { UploadMetadata } from './lib/image-upload/before-upload.interface';
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { ImageUploadModule } from './lib/dist/image-upload.module';
-export { ImageUploadComponent, FileHolder } from './lib/dist/image-upload/image-upload.component';
+export { ImageUploadModule } from './lib/image-upload.module';
+export { ImageUploadComponent, FileHolder } from './lib/image-upload/image-upload.component';
